6356fcda61d4b64fb3bc3a61c3c1b3f1acfc3a4a
Jakub1888 <krasnan.jk@gmail.com>
2023-05-18 17:14:53 +0200

    added tests for button

--- src/app/shared/components/button/button.component.spec.ts
+++ src/app/shared/components/button/button.component.spec.ts
@@ -8,22 +8,37 @@ describe('ButtonComponent', () => {
 	let spectator: Spectator<ButtonComponent>;
 	const createComponent = createComponentFactory({
 		component: ButtonComponent,
-		imports: [
-			MatButtonModule, // Add MatButtonModule
-			MatIconModule, // Add MatIconModule
-			BrowserAnimationsModule // Add BrowserAnimationsModule
-		]
+		imports: [MatButtonModule, MatIconModule, BrowserAnimationsModule]
 	});
 
 	beforeEach(() => {
 		spectator = createComponent();
 	});
 
-	it('should emit buttonClicked event when button is clicked', () => {
-		const buttonClickedSpy = jest.spyOn(spectator.component.buttonClicked, 'emit');
-		spectator.component.onButtonClicked();
+	it('should have the specified label text', () => {
+		const labelText = 'Submit';
+		spectator.setInput('label', labelText);
 
-		expect(buttonClickedSpy).toHaveBeenCalled();
+		expect(spectator.query('button')).toHaveText(labelText);
+	});
+
+	it('should have the specified mat button type', () => {
+		const matBtnType = 'mat-stroked-button';
+		spectator.setInput('matBtnType', matBtnType);
+
+		expect(spectator.query('button')).toHaveClass(matBtnType);
+	});
+
+	it('should be disabled if the disabled property is true', () => {
+		spectator.setInput('disabled', true);
+
+		expect(spectator.query('button')).toBeDisabled();
+	});
+
+	it('should not be disabled if the disabled property is false', () => {
+		spectator.setInput('disabled', false);
+
+		expect(spectator.query('button')).not.toBeDisabled();
 	});
 
 	it('should apply the specified btnColor class to the button', () => {
@@ -33,5 +48,12 @@ describe('ButtonComponent', () => {
 		spectator.setInput('color', btnColor);
 
 		expect(spectator.query('button')).toHaveClass(expectedClassName);
+	});
+
+	it('should emit buttonClicked event when button is clicked', () => {
+		const buttonClickedSpy = jest.spyOn(spectator.component.buttonClicked, 'emit');
+		spectator.component.onButtonClicked();
+
+		expect(buttonClickedSpy).toHaveBeenCalled();
 	});
 });
