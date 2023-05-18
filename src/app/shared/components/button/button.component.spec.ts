1adbd5630c5f048278a0c10ad8a91260fb6eb3f3
Jakub1888 <krasnan.jk@gmail.com>
2023-05-18 06:59:02 +0200

    jest config, adding first tests for button

--- src/app/shared/components/button/button.component.spec.ts
+++ src/app/shared/components/button/button.component.spec.ts
@@ -10,9 +10,18 @@ describe('ButtonComponent', () => {
 	});
 
 	it('should emit buttonClicked event when button is clicked', () => {
-		const buttonClickedSpy = spyOn(spectator.component.buttonClicked, 'emit');
+		const buttonClickedSpy = jest.spyOn(spectator.component.buttonClicked, 'emit');
 		spectator.component.onButtonClicked();
 
 		expect(buttonClickedSpy).toHaveBeenCalled();
+	});
+
+	it('should apply the specified btnColor class to the button', () => {
+		const btnColor = 'warn';
+		const expectedClassName = `mat-${btnColor}-button`;
+
+		spectator.setInput('btnColor', btnColor);
+
+		expect(spectator.query('button')).toHaveClass(expectedClassName);
 	});
 });
