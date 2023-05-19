23159c47b4357f059150606a447370b96309a4dd
Jakub1888 <krasnan.jk@gmail.com>
2023-05-19 08:01:36 +0200

    auth spec file prep

--- src/app/core/auth/auth.component.spec.ts
+++ src/app/core/auth/auth.component.spec.ts
@@ -1,21 +1,23 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { AuthComponent } from './auth.component';
+import { createComponentFactory, Spectator } from '@ngneat/spectator';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ButtonModule } from '../../shared/components/button';
 
 describe('AuthComponent', () => {
-  let component: AuthComponent;
-  let fixture: ComponentFixture<AuthComponent>;
+	let spectator: Spectator<AuthComponent>;
+	const createComponent = createComponentFactory({
+		component: AuthComponent,
+		imports: [ButtonModule, BrowserAnimationsModule]
+	});
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      declarations: [AuthComponent]
-    });
-    fixture = TestBed.createComponent(AuthComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+	beforeEach(() => {
+		spectator = createComponent();
+	});
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+	it('should log "xd" when onAuthFormSubmit is called', () => {
+		const consoleLogSpy = jest.spyOn(console, 'log');
+		spectator.component.onAuthFormSubmit();
+
+		expect(consoleLogSpy).toHaveBeenCalledWith('xd');
+	});
 });
