e35616a7a27fd2f585475c2c270d2339c1e5df0d
jakub krasnan <jakub.krasnan@asseco-ce.com>
2023-06-01 15:01:39 +0200

    nav and auth styling

--- src/app/shared/components/navigation/navigation.module.ts
+++ src/app/shared/components/navigation/navigation.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavigationComponent } from './navigation.component';
 import { RouterModule } from '@angular/router';
+import { ButtonModule } from '../button';
 
 @NgModule({
 	declarations: [NavigationComponent],
-	imports: [CommonModule, RouterModule],
+	imports: [CommonModule, RouterModule, ButtonModule],
 	exports: [NavigationComponent]
 })
 export class NavigationModule {}
